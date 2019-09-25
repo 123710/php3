e5a409b27957d25f29c4c1b6bdf6c9d055629ff9
Bashar <123710@student.drenthecollege.nl>
2019-09-25 09:04:59 +0200

    opdracht5 aangemaakt

---
+++ opdracht5.php
@@ -0,0 +1,11 @@
+<?php
+require 'class5.inc';
+
+
+  $persoon1 = new class5('Bashar', 'Al Badeen','05-01-1995');
+  echo var_dump($persoon1);
+
+  $persoon2 = new class5('Sam', 'Al Badeen','08-05-2018');
+  echo var_dump($persoon2);
+
+ ?>

e5633bb39c2a529cfc58dec95b73f606a18e7c3d
Bashar <123710@student.drenthecollege.nl>
2019-09-25 10:28:59 +0200

    Opdracht 9 af

--- Opdracht9.php
+++ Opdracht9.php
@@ -1,10 +1,11 @@
 <?php
-
-// In de tekst worden de volgende incorrecte voorbeelden gebruikt voor extends:
-// - Query extends MySQL -> kan niet, een query is geen database!
-// - Dier extends Konijn -> kan niet, niet ieder dier is een konijn!
-// Bedenk zelf nog 1 voorbeeld van een incorrecte extends
-  //1: persoon extends student -> kan niet, niet ieder persoon is een student.
-
+echo "
+ In de tekst worden de volgende incorrecte voorbeelden gebruikt voor extends:<br>
+ - Query extends MySQL -> kan niet, een query is geen database!<br>
+ - Dier extends Konijn -> kan niet, niet ieder dier is een konijn!<br>
+ Bedenk zelf nog 1 voorbeeld van een incorrecte extends<br>
+ <br>
+  1: persoon extends student -> kan niet, niet ieder persoon is een student.<br>
+";
 
  ?>

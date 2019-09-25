984b9285aefc84d827cf12aba730078c1e016f39
Bashar <123710@student.drenthecollege.nl>
2019-09-25 10:27:29 +0200

    af

---
+++ Opdracht8.php
@@ -0,0 +1,14 @@
+<?php
+echo "
+ In de tekst worden de volgende correcte voorbeelden gebruikt voor extends:<br>
+ - MySQL extends Database -> kan, MySQL is een type database<br>
+ - Mens extends Dier -> kan, ieder mens is een dier<br>
+ - Dier extends Organisme -> kan, ieder dier is een organisme<br>
+ - Mens extends Organisme -> kan ook, ieder mens is immers een organisme<br>
+ Bedenk zelf nog 3 voorbeelden van een correcte extends.<br>
+<br>
+ 1: boom extends plant -> kan, ieder boom is een plant.<br>
+ 2: auto extends vervoer -> kan, ieder auto is een vervoer.<br>
+ 3: vlieg extends insect -> kan, ieder auto is een insect.<br>
+";
+ ?>
---
+++ Opdracht9.php
@@ -0,0 +1,10 @@
+<?php
+
+// In de tekst worden de volgende incorrecte voorbeelden gebruikt voor extends:
+// - Query extends MySQL -> kan niet, een query is geen database!
+// - Dier extends Konijn -> kan niet, niet ieder dier is een konijn!
+// Bedenk zelf nog 1 voorbeeld van een incorrecte extends
+  //1: persoon extends student -> kan niet, niet ieder persoon is een student.
+
+
+ ?>
